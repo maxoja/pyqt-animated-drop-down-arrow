b6e230d1d3218d7ec2bce9dd13bbbc121454002f
= <->
2018-03-29 12:03:31 +0700

    created a usable animated drop down arrow widget

---
+++ drow-down-arrow.py
@@ -0,0 +1,95 @@
+from PyQt5.QtWidgets import QWidget, QApplication, QPushButton
+from PyQt5.QtGui import QPen, QPainter, QPolygonF, QColor, QBrush
+from PyQt5.QtCore import QTimer, QPointF, Qt, QLineF
+import sys
+
+class Vertice:
+    def __init__(self, x1, y1, x2, y2):
+        self.sx = x1
+        self.dx = x2-x1
+        self.sy = y1
+        self.dy = y2-y1
+
+    def lerp(self, r, scale=1):
+        return scale*(self.sx + self.dx*r), scale*(self.sy + self.dy*r)
+
+
+class DropDownArrow(QWidget):
+    step = 0.01
+
+    def __init__(self, parent=None, size=50, speed=5, color=QColor(0,0,0), selected=False):
+        QWidget.__init__(self, parent)
+
+        self.setFixedSize(size, size)
+        self.speed = speed
+        self.color = color
+        self.selected = selected
+        self.ratio = 1 if selected else 0
+
+        self.verts = [
+            Vertice(0.2, 0.1,   0.9, 0.3),
+            Vertice(0.2, 0.9,   0.1,   0.3),
+            Vertice(0.9, 0.5, 0.5,   0.8)
+        ]
+
+        timer = QTimer(self)
+        timer.timeout.connect(self.onTick)
+        timer.start(20)
+
+    def setSelected(self, selected):
+        self.selected = selected
+
+    def isSelected(self):
+        return self.selected
+
+    def setColor(self, color):
+        assert(isinstance(color, QColor))
+        self.color = color
+
+    def paintEvent(self, event):
+        pen = QPen()
+        pen.setWidth(self.width()/8)
+        pen.setCapStyle(Qt.RoundCap)
+        pen.setColor(self.color)
+
+        brush = QBrush()
+        brush.setColor(self.color)
+        brush.setStyle(Qt.SolidPattern)
+
+        painter = QPainter(self)
+        painter.setPen(pen)
+        painter.setBrush(brush)
+
+        points = [ QPointF(*v.lerp(self.ratio, self.width())) for v in self.verts ]
+        painter.drawLine(QLineF(points[0],points[1]))
+        painter.drawLine(QLineF(points[1],points[2]))
+        painter.drawLine(QLineF(points[2],points[0]))
+
+        poly = QPolygonF(points)
+        painter.drawPolygon(poly)
+
+    def onTick(self):
+        step = self.step
+        speed = self.speed
+
+        if self.selected :
+            self.ratio += step*speed
+            self.ratio = 1 if self.ratio > 1 else self.ratio
+        else :
+            self.ratio -= step*speed
+            self.ratio = 0 if self.ratio < 0 else self.ratio
+
+        self.update()
+
+
+
+if __name__ == '__main__' :
+    app = QApplication(sys.argv)
+
+    w = DropDownArrow()
+    b = QPushButton(w)
+    b.clicked.connect(lambda: w.setSelected(not w.isSelected()))
+    b.move(35, 35)
+    w.show()
+
+    sys.exit(app.exec_())
